57b75b8bee663d0a18f78c2433e01b82f2443658
Brinsby <madfish54@gmail.com>
2019-02-17 17:29:41 -0500

    Added code to readme, cleaned up main program and added comments for improved readability

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # DLP
+This is the code test for deeplocal written by Josh Hnat. It uses python and the tweepy library to count the most 
+popular hashtags used by the official deep local twitter account in the year 2017. It is a regular console application 
+that is simply called by using
+
+`python twit.py`
+
+Values for the date and account are hardcoded as there weren't any stipulations about checking other accounts or 
+changing the period to check for.
+
+This program requires the following to be installed on your system.
+
+`tweepy` `pip install tweepy`
--- twit.py
+++ twit.py
@@ -4,7 +4,7 @@ from tweepy import OAuthHandler
 from datetime import datetime
 from collections import Counter
 
-
+# Twitter API keys and secrets
 consumer_key = 'X2xwBghwKRIE2SPeLvvH1pZ7f'
 consumer_secret = 'zcoFz18rFB2uvs1rUX4pGeibFB2YG6qoP4kl7TBZScleaWP2ul'
 access_token = '1094650909576101888-ngJPWa3ojdJsJbS5dabTlU3G9XDBfO'
@@ -15,15 +15,18 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
+# Set user for later use and create all needed variables
 item = api.get_user("deeplocal")
 hashtags = []
 tweet_count = 0
 
+# Call API and get tweets, starts from most recent tweets and works backward
 for status in Cursor(api.user_timeline, id="deeplocal").items():
     tweet_count += 1
 
+    # Check to see if tweet falls in range (2017) and if so extract the hashtag and store it while checking to see if
+    # we have already stored the hashtag from a previous tweet
     if status.created_at > datetime(2017,1,1,0,0) and status.created_at < datetime(2017,12,31,11,59,59):
-        print(status.created_at)
         if hasattr(status, "entities"):
             entities = status.entities
             if "hashtags" in entities:
@@ -33,12 +36,15 @@ for status in Cursor(api.user_timeline, id="deeplocal").items():
                             hashtag = ent["text"]
                             if hashtag is not None:
                                 hashtags.append(hashtag)
-
+    # Since we are working backwards, once we pass 2017 there is no need to go further
     if status.created_at < datetime(2017,1,1,0,0):
         break
 
-print("Most used hashtags:")
-for item, count in Counter(hashtags).most_common(10):
+# Print out the resulting list using Counter
+print("Hashtag frequency 2017:")
+for item, count in Counter(hashtags).most_common():
     print(item + "\t" + str(count))
 
+# Print out some fun facts
 print("All done. Processed " + str(tweet_count) + " tweets.")
+print(str(len(hashtags)) + " Hashtags used in total from 2017. " + str(len(Counter(hashtags).keys())) + " unique.")
