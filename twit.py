f7e3652fb62eff90ef476cbd968316c660d55b1e
Brinsby <madfish54@gmail.com>
2019-02-17 17:59:34 -0500

    Removed API Keys and secrets as I realized what I did after the fact.

--- twit.py
+++ twit.py
@@ -5,10 +5,7 @@ from datetime import datetime
 from collections import Counter
 
 # Twitter API keys and secrets
-consumer_key = 'X2xwBghwKRIE2SPeLvvH1pZ7f'
-consumer_secret = 'zcoFz18rFB2uvs1rUX4pGeibFB2YG6qoP4kl7TBZScleaWP2ul'
-access_token = '1094650909576101888-ngJPWa3ojdJsJbS5dabTlU3G9XDBfO'
-access_secret = 'gwb8vu1bJG4OUsqx2s6pGbLN5oc9BvKs1N1XSk1j0bIPO'
+# PLACE API KEYS HERE!
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
